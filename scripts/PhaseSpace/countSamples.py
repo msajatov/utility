f4bd10abff17978cefcd130a53f57f227278b54d
msajatov <e1125768@student.tuwien.ac.at>
2020-04-08 11:18:03 +0200

    Working snapshot of countSamples.py

--- scripts/PhaseSpace/countSamples.py
+++ scripts/PhaseSpace/countSamples.py
@@ -7,6 +7,8 @@ import root_numpy as rn
 import ROOT as R
 from ROOT import TFile
 
+from common.Tools.CutObject.CutObject import Cut
+
 import argparse
 import os
 import copy
@@ -18,7 +20,7 @@ def main():
     parser.add_argument('-e', dest='era',  help='Era', choices=["2016", "2017"], required = True)
     args = parser.parse_args()
     
-    filepath = "/afs/hephy.at/work/m/msajatovic/CMSSW_9_4_0/src/dev/utility"
+    filepath = "/afs/cern.ch/work/m/msajatov/private/CMSSW_9_4_0/src/dev/utility"
         
     era = args.era
     channel = args.channel
@@ -30,23 +32,61 @@ def main():
     settings = Settings(channel, era, "keras", "none")
     settings.config_parser = parser
     
-    samples = [sset for sset in parser.samples if (not "_full" in sset.name)]
-    samples = [sset for sset in samples if (not "AR" in sset.name)]
+    samples = [sset for sset in parser.samples if ("_full" in sset.name)]
+    samples = [sset for sset in samples if (not "data" in sset.name)]
+    # samples = [sset for sset in samples if (not "EMB" in sset.name)]
     
     reader = DataReader()
+
+    
+    # if channel == "et":
+    #     correct_id_1 = 3
+    # elif channel == "mt":
+    #     correct_id_1 = 4
+    # else:
+    #     correct_id_1 = 5
+
+    # correct_id_2 = 5
+
+    # subcuts_1 = ["", " & gen_match_1 == {0}".format(correct_id_1), " & gen_match_1 != {0}".format(correct_id_1)]
+    # subcuts_2 = ["", " & gen_match_2 == {0}".format(correct_id_2), " & gen_match_2 != {0}".format(correct_id_2)]
+
+    subcuts_1 = []
+    subcuts_2 = []
+
+    for i in [1,2,3,4,5,6]:
+        subcuts_1 += [" & gen_match_1 == {0}".format(i)]
+        subcuts_2 += [" & gen_match_2 == {0}".format(i)]
+
+    subcuts_1 += [""]
+    subcuts_2 += [""]
     
     for sset in samples:
-#         print sset
+        print sset
 #         df = reader.get_data_for_sample(sset, ["pt_1", "pt_2", "iso_1", "iso_2"])
 #         print df   
-#         print sset.full_path
+        print sset.full_path
         tfile = reader.get_root_tree(sset)
         
         tree = tfile.TauCheck
         
-        sset.cut = sset.cut.switchCutTo("-ANTIISO1-")
+        #sset.cut = sset.cut.switchCutTo("-ANTIISO1-")
+
+        #newcut = Cut("-OS- && -ISO-", channel)
+        #sset.cut = sset.cut + newcut
         
-        print "{0} {1}: {2}".format(sset.name, sset.cut.original, tree.GetEntries(sset.cut.get()))
+        tempstring = ""
+
+        for sc1 in subcuts_1:
+            for sc2 in subcuts_2:
+                completecut = sset.cut.get() + sc1 + sc2
+                completecutname = sset.cut.original + sc1 + sc2
+                #print "{0}; {1}:".format(sset.name, completecutname)
+                val = tree.GetEntries(completecut)
+                #print "Count: {0}".format(val)
+                tempstring += str(val) + ";"
+            print tempstring
+            tempstring = ""
         
 
 if __name__ == '__main__':
